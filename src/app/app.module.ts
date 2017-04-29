5804954752eed2585dc3857a2f6edcd091173230
kissdave <kiss.david@simonyi.bme.hu>
2017-04-29 16:49:46 +0200

    Home screen added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import {HomeComponent} from "./components/home/home.component";
+
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  }
+]
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+@Component({
+  selector: "comp_home",
+  templateUrl: "./home.component.html"
+})
+
+export class HomeComponent {
+
+}
